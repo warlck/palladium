c09910850b830a046c9727080616992fdcde51dc
Adil <addick@me.com>
2023-10-10 20:29:36 +0800

    Implement scratch code to build and write a block on the disk

--- app/tooling/scratch/main.go
+++ app/tooling/scratch/main.go
@@ -4,12 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/warlck/palladium/foundation/blockchain/database"
+	"github.com/warlck/palladium/foundation/blockchain/merkle"
+	"github.com/warlck/palladium/foundation/blockchain/signature"
+	"github.com/warlck/palladium/foundation/blockchain/storage/disk"
 )
 
 func main() {
-	if err := run(); err != nil {
+	if err := writeScratchBlock(); err != nil {
 		log.Fatalln(err)
 	}
 
@@ -87,4 +92,83 @@ func stamp(value any) ([]byte, error) {
 	data := crypto.Keccak256(stamp, v)
 
 	return data, nil
+}
+
+func writeScratchBlock() error {
+	txs := []database.Tx{
+		{
+			ChainID: 1,
+			Nonce:   1,
+			ToID:    "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32",
+			Value:   100,
+			Tip:     50,
+		},
+		{
+			ChainID: 1,
+			Nonce:   2,
+			ToID:    "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32",
+			Value:   100,
+			Tip:     50,
+		},
+	}
+
+	blockTxns := make([]database.BlockTx, len(txs))
+	for i, txn := range txs {
+		blockTx, err := signToBlockTxn(txn, 15)
+		if err != nil {
+			return err
+		}
+		blockTxns[i] = blockTx
+	}
+
+	tree, err := merkle.NewTree(blockTxns)
+	if err != nil {
+		return err
+	}
+
+	beneficiaryID, err := database.ToAccountID("0x6e4397Fc40dA776f1b27edb115C53b7fCd6AABbA")
+	if err != nil {
+		return err
+	}
+	// Construct the block to be written on the disk.
+	block := database.Block{
+		Header: database.BlockHeader{
+			Number:        1,
+			PrevBlockHash: signature.ZeroHash,
+			TimeStamp:     uint64(time.Now().UTC().UnixMilli()),
+			BeneficiaryID: beneficiaryID,
+			Difficulty:    6,
+			MiningReward:  700,
+			StateRoot:     "nil",
+			TrxRoot:       tree.RootHex(), //
+			Nonce:         0,
+		},
+		MerkleTree: tree,
+	}
+
+	blockData := database.NewBlockData(block)
+	d, err := disk.New("zblock/testminer")
+	if err != nil {
+		return err
+	}
+
+	if err := d.Write(blockData); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func signToBlockTxn(tx database.Tx, gas uint64) (database.BlockTx, error) {
+	pk, err := crypto.HexToECDSA("fb07a21977afeb28a23ab6734ee83208af580c6ac6e0a7c1b31e28054a56a270")
+	if err != nil {
+		return database.BlockTx{}, err
+	}
+
+	signedTx, err := tx.Sign(pk)
+	if err != nil {
+		return database.BlockTx{}, err
+	}
+
+	return database.NewBlockTx(signedTx, gas, 1), nil
 }
