765c6c195a234e26e4ff444604f00e9bbaba8585
Adil <addick@me.com>
2023-12-11 15:06:09 +0800

    Adds functionality to validate the newly mined block in state/block.go
    - Same method applies the transactions and mining rewards of the validated block to the blockchain db
    - Implement the block validation logic in the database/block.go

--- foundation/blockchain/database/database.go
+++ foundation/blockchain/database/database.go
@@ -3,9 +3,12 @@
 package database
 
 import (
+	"fmt"
+	"sort"
 	"sync"
 
 	"github.com/warlck/palladium/foundation/blockchain/genesis"
+	"github.com/warlck/palladium/foundation/blockchain/signature"
 )
 
 // Storage interface represents the behavior required to be implemented by any
@@ -58,4 +61,110 @@ func (db *Database) LatestBlock() Block {
 	defer db.mu.RUnlock()
 
 	return db.latestBlock
+}
+
+// HashState returns a hash based on the contents of the accounts and
+// their balances. This is added to each block and checked by peers.
+func (db *Database) HashState() string {
+	accounts := make([]Account, 0, len(db.accounts))
+	db.mu.RLock()
+	{
+		for _, account := range db.accounts {
+			accounts = append(accounts, account)
+		}
+	}
+	db.mu.RUnlock()
+
+	sort.Sort(byAccount(accounts))
+	return signature.Hash(accounts)
+}
+
+// Write adds a new block to the chain.
+func (db *Database) Write(block Block) error {
+	return db.storage.Write(NewBlockData(block))
+}
+
+// UpdateLatestBlock provides safe access to update the latest block.
+func (db *Database) UpdateLatestBlock(block Block) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	db.latestBlock = block
+}
+
+// ApplyMiningReward gives the specififed account the mining reward.
+func (db *Database) ApplyMiningReward(block Block) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	account := db.accounts[block.Header.BeneficiaryID]
+	account.Balance += block.Header.MiningReward
+
+	db.accounts[block.Header.BeneficiaryID] = account
+}
+
+// ApplyTransaction performs the business logic for applying a transaction
+// to the database.
+func (db *Database) ApplyTransaction(block Block, tx BlockTx) error {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	// Capture these accounts from the database.
+	from, exists := db.accounts[tx.FromID]
+	if !exists {
+		from = newAccount(tx.FromID, 0)
+	}
+
+	to, exists := db.accounts[tx.ToID]
+	if !exists {
+		to = newAccount(tx.ToID, 0)
+	}
+
+	bnfc, exists := db.accounts[block.Header.BeneficiaryID]
+	if !exists {
+		bnfc = newAccount(block.Header.BeneficiaryID, 0)
+	}
+
+	// The account needs to pay the gas fee regardless. Take the
+	// remaining balance if the account doesn't hold enough for the
+	// full amount of gas. This is the only way to stop bad actors.
+	gasFee := tx.GasPrice * tx.GasUnits
+	if gasFee > from.Balance {
+		gasFee = from.Balance
+	}
+	from.Balance -= gasFee
+	bnfc.Balance += gasFee
+
+	// Make sure these changes get applied.
+	db.accounts[tx.FromID] = from
+	db.accounts[block.Header.BeneficiaryID] = bnfc
+
+	// Perform basic accounting checks.
+	{
+		if tx.Nonce != (from.Nonce + 1) {
+			return fmt.Errorf("transaction invalid, wrong nonce, got %d, exp %d", tx.Nonce, from.Nonce+1)
+		}
+
+		if from.Balance == 0 || from.Balance < (tx.Value+tx.Tip) {
+			return fmt.Errorf("transaction invalid, insufficient funds, bal %d, needed %d", from.Balance, (tx.Value + tx.Tip))
+		}
+	}
+
+	// Update the balances between the two parties.
+	from.Balance -= tx.Value
+	to.Balance += tx.Value
+
+	// Give the beneficiary the tip.
+	from.Balance -= tx.Tip
+	bnfc.Balance += tx.Tip
+
+	// Update the nonce for the next transaction check.
+	from.Nonce = tx.Nonce
+
+	// Update the final changes to these accounts.
+	db.accounts[tx.FromID] = from
+	db.accounts[tx.ToID] = to
+	db.accounts[block.Header.BeneficiaryID] = bnfc
+
+	return nil
 }
--- foundation/blockchain/state/block.go
+++ foundation/blockchain/state/block.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 
 	"github.com/warlck/palladium/foundation/blockchain/database"
-	"github.com/warlck/palladium/foundation/blockchain/signature"
 )
 
 // ErrNoTransactions is returned when a block is requested to be created
@@ -42,7 +41,7 @@ func (s *State) MineNewBlock(ctx context.Context) (database.Block, error) {
 		Difficulty:    difficulty,
 		MiningReward:  s.genesis.MiningReward,
 		PrevBlock:     s.db.LatestBlock(),
-		StateRoot:     signature.ZeroHash,
+		StateRoot:     s.db.HashState(),
 		Trxs:          trxs,
 		EvHandler:     s.evHandler,
 	})
@@ -51,15 +50,66 @@ func (s *State) MineNewBlock(ctx context.Context) (database.Block, error) {
 		return database.Block{}, err
 	}
 
-	// // Just check one more time we were not cancelled.
-	// if ctx.Err() != nil {
-	// 	return database.Block{}, ctx.Err()
-	// }
+	// Just check one more time we were not cancelled.
+	if ctx.Err() != nil {
+		return database.Block{}, ctx.Err()
+	}
 
-	for _, tx := range trxs {
-		s.evHandler("state: Remove Tx[%s]", tx)
-		s.mempool.Delete(tx)
+	s.evHandler("state: MineNewBlock: MINING: validate and update database")
+
+	// Validate the block and then update the blockchain database.
+	if err := s.validateUpdateDatabase(block); err != nil {
+		return database.Block{}, err
 	}
 
 	return block, nil
+}
+
+// =============== ==============================================================
+
+// validateUpdateDatabase takes the block and validates the block against the
+// consensus rules. If the block passes, then the state of the node is updated
+// including adding the block to disk.
+func (s *State) validateUpdateDatabase(block database.Block) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.evHandler("state: validateUpdateDatabase: validate block")
+
+	// CORE NOTE: Could add logic to determine if this block was mined by this
+	// node or a peer. If the block is mined by this node, even if a peer beat
+	// me to this function for the same block number, I could replace the peer
+	// block with my own and attempt to have other peers accept my block instead.
+
+	if err := block.ValidateBlock(s.db.LatestBlock(), s.db.HashState(), s.evHandler); err != nil {
+		return err
+	}
+
+	s.evHandler("state: validateUpdateDatabase: write to disk")
+
+	// Write the new block to the chain on disk.
+	if err := s.db.Write(block); err != nil {
+		return err
+	}
+
+	s.db.UpdateLatestBlock(block)
+
+	s.evHandler("state: validateUpdateDatabase: update accounts and remove from mempool")
+	// Process the transactions and update the accounts.
+	for _, tx := range block.MerkleTree.Values() {
+		s.evHandler("state: validateUpdateDatabase: tx[%s] update and remove", tx)
+
+		// Remove this transaction from the mempool.
+		s.mempool.Delete(tx)
+
+		// Apply the balance changes based on this transaction.
+		if err := s.db.ApplyTransaction(block, tx); err != nil {
+			s.evHandler("state: validateUpdateDatabase: WARNING : %s", err)
+			continue
+		}
+	}
+
+	// Apply the mining reward for this block.
+	s.db.ApplyMiningReward(block)
+
+	return nil
 }
